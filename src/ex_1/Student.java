7d7262b0fe621982b0dbd5572513b58288cc0b6f
ShashankBethi <shashankbethi@gmail.com>
2024-12-09 15:42:08 +0530

    3. Commit

---
+++ src/ex_1/Student.java
@@ -0,0 +1,24 @@
+package ex_1;
+
+public class Student {
+	
+	
+	int id;
+	String name;
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+
+}
--- src/sb/for_git/SbForGit1.java
+++ src/sb/for_git/SbForGit1.java
@@ -1,9 +1,16 @@
 package sb.for_git;
 
+import ex_1.Student;
+
 public class SbForGit1 {
 
 	public static void main(String[] args) {
-System.out.println("git");
+		System.out.println("git");
+		
+		
+		
 	}
+	
+	Student student=new Student(101,"ram");
 
 }
